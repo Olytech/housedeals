587ff48676ff47408f318b9fccc5c97b3df28ea9
Olytech Design <adams.cosmas@gmail.com>
2021-07-31 04:25:53 -0700

    updated dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 
-Route::view('/home', 'home')->middleware('auth')->name('home');
+Route::view('/home', 'home')->name('home');
 Route::group(['middleware' => 'auth:admin'], function () {
 });
 
